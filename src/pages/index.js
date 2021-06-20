1b7bac2e245872434bc5bcca56f5ffad7f304ff7
Renan Drechsler <renanvz@outlook.com>
2021-06-20 15:29:16 -0300

    Initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Layout from '../components/Layout/Layout';
+import "./home.css"
+
+export default function Home() {
+  return (
+    <Layout>
+    <section className="header">
+      <div>
+        <h2>Design</h2> 
+        <h3>Develop & Deploy</h3>
+        <p>UX designer & Web Developer</p>
+      </div>
+    </section>
+    </Layout>
+  )
+}
